babe79b636514a1e49c405daef2e876cc9e22ada
Anjaa84 <49702955+Anjaa84@users.noreply.github.com>
2019-08-10 15:24:10 +0530

    Cost Guide repository intial

--- agro_service/src/main/java/com/agro/service/Repository/CostRepository.java
+++ agro_service/src/main/java/com/agro/service/Repository/CostRepository.java
@@ -2,7 +2,9 @@ package com.agro.service.Repository;
 
 import com.agro.service.Domain.CostGuide.Cost;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "cost", path = "cost")
 public interface CostRepository  extends MongoRepository<Cost, String> {
 
 
